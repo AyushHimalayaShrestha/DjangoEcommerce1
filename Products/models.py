eefc786b48bb49d524db3ba20e55acc838117050
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-26 12:26:17 +0545

    Creating Model

--- Products/models.py
+++ Products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=115)
+    product_price = models.FloatField()
+    stock = models.IntegerField()
+    image_url = models.CharField(max_length=255)
+    product_description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
