2e6c861a614183610bd4fb9ee738c2b8afee81e4
AyushHimalayaShrestha <aayush625@gmail.com>
2025-03-31 21:36:56 +0545

    static folder

--- Products/models.py
+++ Products/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     product_name = models.CharField(max_length=115)
     product_price = models.FloatField()
     stock = models.IntegerField()
-    image_url = models.CharField(max_length=255)
+    product_image= models.FileField()
     product_description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     category= models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
